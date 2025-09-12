f4fe7524ce86cbbd4244ec82e8aef63bdf313911
gnat <hellunatalia@gmail.com>
2025-09-12 12:29:36 -0700

    added more code to script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let hungry = true;
-let x = true;
 
-console.log(`It's ${hungry} that I'm hungry for ${x}`);
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id    
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3)
+    console.log(randomNumber)
+}
