bd808b584817b3b1dda6793a6061d9cb46ba4b05
gnat <hellunatalia@gmail.com>
2025-09-29 14:25:42 -0700

    added pong to script.js

--- script.js
+++ script.js
@@ -1,17 +1,133 @@
-
+// ROCK-PAPER-SCISSORS
 const computerChoiceDisplay = document.getElementById('computer-choice');
 const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id    
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3)
-    console.log(randomNumber)
+
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
 }
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'draw!'
+    }
+
+        if (computerChoice === 'rock' && userChoice === 'paper') {
+        result = 'you win!'
+    }
+
+            if (computerChoice === 'rock' && userChoice === 'scissors') {
+        result = 'you lost...'
+    }
+
+
+            if (computerChoice === 'paper' && userChoice === 'scissors') {
+        result = 'you win!'
+    }
+
+            if (computerChoice === 'paper' && userChoice === 'rock') {
+        result = 'you lost...'
+    }
+
+            if (computerChoice === 'scissors' && userChoice === 'rock') {
+        result = 'you win!'
+    }
+
+            if (computerChoice === 'scissors' && userChoice === 'paper') {
+        result = 'you lost...'
+    }
+
+    resultDisplay.innerHTML = result
+}
+// RPS END
+
+
+
+
+// PONG
+
+
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtnPong = document.querySelector("#resetBtnPong")
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "pink";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black";
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+    width: 25,
+    height: 100,
+    x: 0,
+    y: 0,
+};
+let paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100
+};
+
+window.addEventListener("keydown", changeDirection);
+resetBtnPong.addEventListener("click", resetGame);
+
+gameStart();
+drawPaddles();
+
+function gameStart(){};
+function nextTick(){};
+function clearBoard(){};
+function drawPaddles(){
+    ctx.strokeStyle = paddleBorder;
+
+    ctx.fillStyle = paddle1Color;
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+};
+function createBall(){};
+function moveBall(){};
+function drawBall(){};
+function checkCollision(){};
+function changeDirection(){};
+function updateScore(){};
+function resetGame(){};
