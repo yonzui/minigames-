dffbdded13e36c9137ba14c4873188f40300c593
gnat <hellunatalia@gmail.com>
2025-10-19 23:06:52 -0700

    updated memory game code

--- script.js
+++ script.js
@@ -1,387 +1,146 @@
 const selectors = {
-    boardContainer : document.querySelector('.board-container'),
-    board : document.querySelector('.board'),
-    moves : document.querySelector('.moves'),
-    timer : document.querySelector('.timer'),
-    start : document.querySelector('button'),
-    win : document.querySelector('.win')
-};
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
 
 const state = {
-    gameStarted : false,
-    flippedCards : 0,
-    totalFlips : 0,
-    totalTime : 0,
-    loop : null
-};
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null
+}
 
 const shuffle = array => {
-    const clonedArray = [...array];
+    const clonedArray = [...array]
 
-    for(let i = clonedArray.length - 1; i > 0; i--){
-        const randomIndex = Math.floor(Math.random() * (i + 1));
-        const original = clonedArray[i];
+    for (let i = clonedArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1))
+        const original = clonedArray[i]
 
-        clonedArray[i] = clonedArray[randomIndex];
-        clonedArray[randomIndex] = original;
+        clonedArray[i] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
     }
-    return clonedArray;
+
+    return clonedArray
 }
 
 const pickRandom = (array, items) => {
-    const clonedArray = [...array];
-    const randomPicks = [];
+    const clonedArray = [...array]
+    const randomPicks = []
 
-    for(let i=0; i<items; i++){
-        const randomIndex = Math.floor(Math.random() * clonedArray.length);
-
-        randomPicks.push(clonedArray[randomIndex]);
-        clonedArray.splice(randomIndex, 1);
+    for (let i = 0; i < items; i++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
     }
-    return randomPicks;
+
+    return randomPicks
 }
 
 const generateGame = () => {
-    const dimensions = selectors.board.getAttribute('data-dimension');
+    const dimensions = selectors.board.getAttribute('data-dimension')  
 
-    if(dimensions % 2 !== 0){
+    if (dimensions % 2 !== 0) {
         throw new Error("The dimension of the board must be an even number.")
     }
 
     const emojis = ['🥔', '🍒', '🥑', '🌽', '🥕', '🍇', '🍉', '🍌', '🥭', '🍍']
-    const picks = pickRandom(emojis, (dimensions * dimensions) / 2);
-    const items = shuffle([...picks, ...picks]);
-    const cards =  `
+    const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
+    const items = shuffle([...picks, ...picks])
+    const cards = `
         <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
             ${items.map(item => `
-                    <div class="card">
-                        <div class="card-front"></div>
-                        <div class="card-back">${item}</div>
-                    </div>
-                `).join('')}
-        </div>
+                <div class="card">
+                    <div class="card-front"></div>
+                    <div class="card-back">${item}</div>
+                </div>
+            `).join('')}
+       </div>
     `
+    
+    const parser = new DOMParser().parseFromString(cards, 'text/html')
 
-    const parser = new DOMParser().parseFromString(cards, 'text/html');
     selectors.board.replaceWith(parser.querySelector('.board'))
-};
+}
 
 const startGame = () => {
-    state.gameStarted = true;
-    selectors.start.classList.add('disabled');
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
 
     state.loop = setInterval(() => {
-        state.totalTime++;
+        state.totalTime++
 
-        selectors.moves.innerText = `${state.totalFlips} moves`;
-        selectors.timer.innerText = `Time`
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `Time: ${state.totalTime} sec`
+    }, 1000)
+}
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card:not(.matched)').forEach(card => {
+        card.classList.remove('flipped')
+    })
+
+    state.flippedCards = 0
+}
+
+const flipCard = card => {
+    state.flippedCards++
+    state.totalFlips++
+
+    if (!state.gameStarted) {
+        startGame()
+    }
+
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped')
+    }
+
+    if (state.flippedCards === 2) {
+        const flippedCards = document.querySelectorAll('.flipped:not(.matched)')
+
+        if (flippedCards[0].innerText === flippedCards[1].innerText) {
+            flippedCards[0].classList.add('matched')
+            flippedCards[1].classList.add('matched')
+        }
+
+        setTimeout(() => {
+            flipBackCards()
+        }, 1000)
+    }
+    if (!document.querySelectorAll('.card:not(.flipped)').length) {
+        setTimeout(() => {
+            selectors.boardContainer.classList.add('flipped')
+            selectors.win.innerHTML = `
+                <span class="win-text">
+                    You won!<br />
+                    with <span class="highlight">${state.totalFlips}</span> moves<br />
+                    under <span class="highlight">${state.totalTime}</span> seconds
+                </span>
+            `
+
+            clearInterval(state.loop)
+        }, 1000)
+    }
+}
+
+const attachEventListeners = () => {
+    document.addEventListener('click', event => {
+        const eventTarget = event.target
+        const eventParent = eventTarget.parentElement
+
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped')) {
+            flipCard(eventParent)
+        } else if (eventTarget.nodeName === 'BUTTON' && !eventTarget.className.includes('disabled')) {
+            startGame()
+        }
     })
 }
 
-
-
-
-
-
-// MEMORY GAME END
-
-
-
-
-
-
-
-
-// ROCK-PAPER-SCISSORS
-const gameContainer = document.querySelector(".container"),
-userResult = document.querySelector(".user_result img"),
-cpuResult = document.querySelector(".cpu_result img"),
-result = document.querySelector(".result"),
-optionImages = document.querySelectorAll(".option_image");
-
-
-optionImages.forEach((image, index) => {
-    image.addEventListener("click", (e) => {
-        image.classList.add("active");
-
-        userResult.src = cpuResult.src = "ASSETS/images/rock.png"
-        result.textContent = "wait..."
-
-
-        optionImages.forEach ((image2, index2) => {
-
-            
-            index !== index2 && image2.classList.remove("active");
-        });
-
-        gameContainer.classList.add("start");
-
-        
-        let time = setTimeout(() =>{
-            gameContainer.classList.remove("start");
-
-        let imageSrc = e.target.querySelector("img").src;
-
-        userResult.src = imageSrc;
-
-        
-        let randomNumber = Math.floor(Math.random() * 3);
-
-        let cpuImages = ["ASSETS/images/rock.png", "ASSETS/images/paper.png", "ASSETS/images/scissors.png"];
-        cpuResult.src = cpuImages[randomNumber];
-
-
-
-        let cpuValue = ["R", "P", "S"][randomNumber];
-        
-        let userValue = ["R", "P", "S"][index];
-
-
-      let outcomes = {
-        RR: "draw",
-        RP: "computer",
-        RS: "you",
-        PP: "draw!",
-        PR: "you",
-        PS: "computer",
-        SS: "draw",
-        SR: "computer",
-        SP: "you",
-      };
-      
-      
-      let outComeValue = outcomes[userValue + cpuValue];
-
-      result.textContent = userValue === cpuValue ? "match draw..." : `${outComeValue} won!`
-      console.log(outComeValue);
-      
-        },2500)
-
-    });
-});
-
-
-
-
-
-// RPS END
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// PONG
-
-
-const gameBoard = document.querySelector("#gameBoard");
-const ctx = gameBoard.getContext("2d");
-const scoreText = document.querySelector("#scoreText");
-const resetBtnPong = document.querySelector("#resetBtnPong")
-const gameWidth = gameBoard.width;
-const gameHeight = gameBoard.height;
-const boardBackground = "white";
-const paddle1Color = "white";
-const paddle2Color = "white";
-const paddleBorder = "black";
-const ballColor = "red";
-const ballBorderColor = "darkred";
-const ballRadius = 12.5;
-const paddleSpeed = 50;
-let intervalID;
-let ballSpeed = 1;
-let ballX = gameWidth / 2;
-let ballY = gameHeight / 2;
-let ballXDirection = 0;
-let ballYDirection = 0;
-let player1Score = 0;
-let player2Score = 0;
-let paddle1 = {
-    width: 25,
-    height: 100,
-    x: 0,
-    y: 0,
-};
-let paddle2 = {
-    width: 25,
-    height: 100,
-    x: gameWidth - 25,
-    y: gameHeight - 100
-};
-
-window.addEventListener("keydown", changeDirection);
-resetBtnPong.addEventListener("click", resetGame);
-
-gameStart();
-
-
-function gameStart(){
-    createBall();
-    nextTick();
-};
-function nextTick(){
-    intervalID = setTimeout(() => {
-       clearBoard(); 
-       drawPaddles();
-       moveBall();
-       drawBall(ballX, ballY);
-       checkCollision();
-       nextTick();
-    }, 10)
-};
-function clearBoard(){
-    ctx.fillStyle = boardBackground;
-    ctx.fillRect(0, 0, gameWidth, gameHeight);
-};
-function drawPaddles(){
-    ctx.strokeStyle = paddleBorder;
-
-    ctx.fillStyle = paddle1Color;
-    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
-
-    ctx.fillStyle = paddle2Color;
-    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
-
-};
-
-function createBall(){
-    ballSpeed = 1;
-    if(Math.round(Math.random()) == 1){
-        ballXDirection = 1;
-    }
-    else{
-        ballXDirection = -1;
-    }
-    if(Math.round(Math.random()) == 1){
-        ballYDirection = 1;
-    }
-    else{
-        ballYDirection = -1;
-    }
-    ballX = gameWidth / 2;
-    ballY = gameHeight / 2;
-    drawBall(ballX, ballY);
-};
-function moveBall(){
-    ballX += (ballSpeed * ballXDirection);
-    ballY += (ballSpeed * ballYDirection);
-};
-function drawBall(ballX, ballY){
-    ctx.fillStyle = ballColor;
-    ctx.strokeStyle = ballBorderColor;
-    ctx.lineWidth = 2;
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fill();
-};
-function checkCollision(){
-    if(ballY <= 0 + ballRadius){
-        ballYDirection *= -1;
-    }
-    if(ballY >= gameHeight - ballRadius){
-        ballYDirection *= -1;
-    } 
-    if(ballX <= 0){
-        player2Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX >= gameWidth){
-        player1Score+=1;
-        updateScore();
-        createBall();
-        return;
-    }
-    if(ballX <=(paddle1.x + paddle1.width + ballRadius)){
-        if(ballY > paddle1.y && ballY < paddle1.y + paddle1.height){
-            ballX = (paddle1.x + paddle1.width) + ballRadius;
-            ballXDirection *= -1;
-            ballSpeed +=1;
-        }
-    }
-    if(ballX >=(paddle2.x - ballRadius)){
-        if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
-            ballX = paddle2.x - ballRadius;
-            ballXDirection *= -1;
-            ballSpeed +=1;
-        }
-    }
-};
-function changeDirection(event){
-    const keyPressed = event.keyCode;
-    const paddle1Up = 87;
-    const paddle1Down = 83;
-    const paddle2Up = 38;
-    const paddle2Down = 40;
-
-    switch(keyPressed){
-        case(paddle1Up):
-            if(paddle1.y > 0){
-                paddle1.y -= paddleSpeed;
-            }
-                break;
-        case(paddle1Down):
-            if(paddle1.y < gameHeight - paddle1.height){
-                paddle1.y += paddleSpeed;
-            }
-            break;
-        case(paddle2Up):
-            if(paddle2.y > 0){
-                paddle2.y -= paddleSpeed;
-            }
-            break;
-        case(paddle2Down):
-            if(paddle2.y < gameHeight - paddle2.height){
-                paddle2.y += paddleSpeed;
-            }
-    }
-};
-function updateScore(){
-    scoreText.textContent = `${player1Score} : ${player2Score}`;
-};
-function resetGame(){
-    player1Score = 0;
-    player2Score = 0;
-    paddle1 = {
-        width: 25,
-        height: 100,
-        x: 0,
-        y: 0
-    };
-    paddle2 = {
-        width: 25,
-        height: 100,
-        x: gameWidth - 25,
-        y: gameHeight - 100
-    };
-    ballSpeed = 1;
-    ballX = 0;
-    ballY = 0;
-    ballXDirection = 0;
-    ballYDirection = 0;
-    updateScore();
-    clearInterval(intervalID);
-    gameStart();
-};
-
-// PONG END
-
-
-
-
-
-// MG START
-
+generateGame()
+attachEventListeners()
